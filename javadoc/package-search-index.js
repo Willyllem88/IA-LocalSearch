87a1c500783b4aad9384a1fa87824287924c2348
Willyllem88 <guillemcabre2004@gmail.com>
2024-10-06 09:46:42 +0200

    javadoc inicial, pero ara funciona

--- javadoc/package-search-index.js
+++ javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"implementation"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"IA.Azamon"}];updateSearchResults();
